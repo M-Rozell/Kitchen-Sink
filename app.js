9ebb5a78b73a3aac1e0ed822a64a8c4b681ccfe2
M-Rozell <95882154+M-Rozell@users.noreply.github.com>
2022-01-20 15:51:35 -0600

    added function to check age

--- app.js
+++ app.js
@@ -1,10 +1,36 @@
-var myName = "Maggie";
+//var myName = "Maggie";
 
-const numberStates = 50;
+//const numberStates = 50;
 
-myName.numberStates
+//myName.numberStates
 //addin my name and number of states
 
 function sayHello() {
     alert("Hello World!");
 }
+console.log(sayHello());
+
+var person = checkAge("Charles", 21);
+var person = checkAge("Abby", 27);
+var person = checkAge('James', 18);
+var person = checkAge('John', 17);
+
+function checkAge(name,age) {
+    if(age<21) {
+        alert("Sorry " + name + ", you're not allowed to view this page!");
+    }
+ 
+}   
+
+console.log(checkAge());
+
+ 
+      
+
+   
+
+
+     
+        
+    
+    
