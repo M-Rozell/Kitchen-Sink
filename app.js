7f9942105c7816df67f799b10b45a758ed291484
M-Rozell <95882154+M-Rozell@users.noreply.github.com>
2022-01-21 13:36:20 -0600

    added array of objects and used checkAge function to scan array.

--- app.js
+++ app.js
@@ -1,43 +1,97 @@
-//var myName = "Maggie";
+var myName = "Maggie";
 
-//const numberStates = 50;
+const numberStates = 50;
 
-//console.log(myName + numberStates);
+var nameNumber=(myName + numberStates);
 //addin my name and number of states
 
 function sayHello() {
     alert("Hello World!");
 }
-console.log(sayHello());
+sayHello()
 
 var person = checkAge("Charles", 21);
 var person = checkAge("Abby", 27);
 var person = checkAge('James', 18);
 var person = checkAge('John', 17);
 
-function checkAge(name,age) {
-    if(age<21) {
+
+function checkAge(name, age) {
+    if (age < 21) {
         alert("Sorry " + name + ", you're not allowed to view this page!");
     }
-};   
-console.log(checkAge());
+};
 
-var veggies=['Brussles', 'Broccoli', 'Peas', 'Asparagus'];
-var count = 0;
-while(count<veggies.length) {
-    console.log(veggies[count]);
-    count++;
+
+var veggies = ['Brussles', 'Broccoli', 'Peas', 'Asparagus'];
+for (i=0; i<veggies.length; i++) {
+    console.log(veggies[i]);
 }
 
-
+var pet = {
+    name: "Oliver",
+    breed: "Feral",
+}
+console.log(pet);
 
  
-      
+var myArray = [
+    {name: 'maggie',
+        age: 40
+        },
+    {name: 'Stewart',
+        age: 15
+        },
+    {name: 'Cat',
+        age: 45
+    },
+    {name: 'Robert',
+        age: 19
+    },
+    {name: 'Zach',
+        age: 20
+    }
+    
+];
+
+for (i=0; i<myArray.length; i++)
+checkAge(myArray[i].name, myArray[i].age);
+
+
+
+function getLength(string) {
+    return string.length; 
+    
+}
+
+var num = getLength('Hello World');
 
    
-
-
-     
-        
+ if(num % 2 == 0) {
+       console.log('The world is nice and even!');
+   } else {
+       console.log('The world is an odd place!');
+   }
     
     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
